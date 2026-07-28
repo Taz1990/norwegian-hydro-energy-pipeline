7ab8913017867bd148185eca56c3dbd9f85f874e
Taz Ahmed <tazete90@gmail.com>
2026-07-28 13:54:36 +0200

    Simplified stg_stations to match Snowflake table structure

--- dbt/hydro_project/models/staging/stg_stations.sql
+++ dbt/hydro_project/models/staging/stg_stations.sql
@@ -1,16 +1,6 @@
 {{ config(materialized='view') }}
 
 select
-    station_id,
-    station_name,
-    river_name,
-    county,
-    latitude::float as latitude,
-    longitude::float as longitude,
-    elevation_m::float as elevation_m,
-    active,
-    created_at,
-    updated_at
-from {{ source('postgres', 'stations') }}
-where latitude is not null
-  and longitude is not null
+    STATION_ID,
+    STATION_NAME
+from {{ source('raw', 'STATIONS') }}
